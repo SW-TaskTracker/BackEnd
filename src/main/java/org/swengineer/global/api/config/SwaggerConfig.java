cb1022240a85b2a9c68c1d52575e7d97f27375bc
alissa159 <seohyunyun325@gmail.com>
2026-05-24 12:47:58 +0900

    refactor: 스웨거 설정에 Authorization 추가

--- src/main/java/org/swengineer/global/api/config/SwaggerConfig.java
+++ src/main/java/org/swengineer/global/api/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package org.swengineer.global.api.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,10 +14,21 @@ import java.util.List;
 @Configuration
 public class SwaggerConfig {
 
+    private static final String BEARER_AUTH = "BearerAuth";
+
     @Bean
     public OpenAPI openAPI() {
         return new OpenAPI()
-                .components(new Components())
+                .components(new Components()
+                        .addSecuritySchemes(BEARER_AUTH, new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(BEARER_AUTH))
                 .info(apiInfo())
                 .servers(List.of(
                         new Server().url("https://loutiner.p-e.kr").description("운영 서버"),
